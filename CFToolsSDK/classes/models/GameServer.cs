fc36a2a6a6d3110ee317ca429ae7cf5aef5085f5
Krypton91 <krypton1@gmx.at>
2022-09-19 20:40:48 +0200

    added Gameserver object.

---
+++ CFToolsSDK/classes/models/GameServer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CFToolsSDK.classes.models
+{
+    public class GameServer
+    {
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+        public string error { get; set; }
+        public bool experimental { get; set; }
+        public string hive { get; set; }
+        public bool modded { get; set; }
+        public bool official { get; set; }
+        public string shard { get; set; }
+        public bool whitelist { get; set; }
+        public bool isFPPEnabled { get; set; }
+        public bool isTPPEnabled { get; set; }
+        public string time { get; set; }
+        public string time_acceleration_general { get; set; }
+        public string time_acceleration_night { get; set; }
+
+        public GeoLocation location { get; set; }
+        public List<Mod> mods { get; set; }
+        public status status { get; set; }
+        public string map_name { get; set; }
+        public int Rank { get; set; }
+        public int Rating { get; set; }
+        public bool isPasswordProtected { get; set; }
+        public bool isBattleyeEnabled { get; set; }
+        public bool vac { get; set; }
+        public int slots { get; set; }
+        public string version { get; set; }
+    }
+
+    public class GeoLocation
+    {
+        public bool available { get; set; }
+        public string city_name { get; set; }
+        public string city_region { get; set; }
+        public string continent { get; set; }
+        public string country_Code { get; set; }
+        public string country_Name { get; set; }
+        public string timezone { get; set; }
+    }
+
+    public class Mod
+    {
+        public int file_id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class status
+    {
+        public bool bots { get; set; }
+        public int players { get; set; }
+        public bool Queue { get; set; }
+        public int QueueCount { get; set; }
+    }
+}
