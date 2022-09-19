81173a6b6502a4d54b8f50f564d801e9dc936a8f
Krypton91 <krypton1@gmx.at>
2022-09-19 19:36:22 +0200

    Added Leaderboard object.

---
+++ CFToolsSDK/classes/models/Leaderboard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CFToolsSDK.classes.models
+{
+    public class Leaderboard
+    {
+        public string cftools_id { get; set; }
+        public int deaths { get; set; }
+        public int hits { get; set; }
+        public double kdratio { get; set; }
+        public int kills { get; set; }
+        public string latest_name { get; set; }
+        public double longest_kill { get; set; }
+        public double longest_shot { get; set; }
+        public int playtime { get; set; }
+        public int rank { get; set; }
+        public int suicides { get; set; }
+        public int? environment_deaths { get; set; }
+        public int? infected_deaths { get; set; }
+        public int? falldamage_deaths { get; set; }
+    }
+
+    public class LeaderboardResponse
+    {
+        public List<Leaderboard> leaderboard { get; set; }
+        public bool status { get; set; }
+    }
+}
