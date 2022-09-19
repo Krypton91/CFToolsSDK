32b53d7eab47ad36019baf78b99bebae240800b8
Krypton91 <krypton1@gmx.at>
2022-09-19 18:49:05 +0200

    added Config Manager.

---
+++ CFToolsSDK/classes/config/CFToolsConfigManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+
+namespace CFToolsSDK.classes.config
+{
+    internal class CFToolsConfigManager
+    {
+        internal string Application_id { get; set; }
+        internal string secret { get; set; }
+
+        internal CFToolsConfigManager()
+        {
+            this.Application_id = ConfigurationManager.AppSettings["Application_id"];
+            this.secret = ConfigurationManager.AppSettings["secret"];
+        }
+    }
+}
